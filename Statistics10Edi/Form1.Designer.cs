3ad889ae30c076b8caa3bd93cd51394b7eef607f
PC-22 <it.pgee.burgas@gmail.com>
2023-11-20 17:47:56 +0200

    Design change

--- Statistics10Edi/Form1.Designer.cs
+++ Statistics10Edi/Form1.Designer.cs
@@ -28,26 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtNum = new System.Windows.Forms.TextBox();
             this.btnAvr = new System.Windows.Forms.Button();
             this.lblAvr = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtNum
             // 
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.txtNum.Location = new System.Drawing.Point(281, 34);
             this.txtNum.Name = "txtNum";
-            this.txtNum.Size = new System.Drawing.Size(403, 22);
+            this.txtNum.Size = new System.Drawing.Size(493, 45);
             this.txtNum.TabIndex = 0;
             // 
             // btnAvr
             // 
+            this.btnAvr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnAvr.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnAvr.Location = new System.Drawing.Point(75, 118);
             this.btnAvr.Name = "btnAvr";
-            this.btnAvr.Size = new System.Drawing.Size(104, 53);
+            this.btnAvr.Size = new System.Drawing.Size(120, 53);
             this.btnAvr.TabIndex = 1;
-            this.btnAvr.Text = "button1";
-            this.btnAvr.UseVisualStyleBackColor = true;
+            this.btnAvr.Text = "Averge";
+            this.btnAvr.UseVisualStyleBackColor = false;
             this.btnAvr.Click += new System.EventHandler(this.btnAvr_Click);
             // 
             // lblAvr
@@ -59,11 +64,25 @@
             this.lblAvr.Size = new System.Drawing.Size(0, 39);
             this.lblAvr.TabIndex = 2;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(11, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 47);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter numbers:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblAvr);
             this.Controls.Add(this.btnAvr);
             this.Controls.Add(this.txtNum);
@@ -80,6 +99,7 @@
         private System.Windows.Forms.TextBox txtNum;
         private System.Windows.Forms.Button btnAvr;
         private System.Windows.Forms.Label lblAvr;
+        private System.Windows.Forms.Label label1;
     }
 }
 
