a6113f5daa68c38fab5e4893791c486aa464ff6a
PC-22 <it.pgee.burgas@gmail.com>
2023-11-20 18:04:37 +0200

    Median

--- Statistics10Edi/Form1.Designer.cs
+++ Statistics10Edi/Form1.Designer.cs
@@ -33,23 +33,27 @@
             this.btnAvr = new System.Windows.Forms.Button();
             this.lblAvr = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnMed = new System.Windows.Forms.Button();
+            this.lblMed = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtNum
             // 
             this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtNum.Location = new System.Drawing.Point(281, 34);
+            this.txtNum.Location = new System.Drawing.Point(269, 26);
+            this.txtNum.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNum.Name = "txtNum";
-            this.txtNum.Size = new System.Drawing.Size(493, 45);
+            this.txtNum.Size = new System.Drawing.Size(371, 38);
             this.txtNum.TabIndex = 0;
             // 
             // btnAvr
             // 
             this.btnAvr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btnAvr.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnAvr.Location = new System.Drawing.Point(75, 118);
+            this.btnAvr.Location = new System.Drawing.Point(56, 96);
+            this.btnAvr.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAvr.Name = "btnAvr";
-            this.btnAvr.Size = new System.Drawing.Size(120, 53);
+            this.btnAvr.Size = new System.Drawing.Size(90, 43);
             this.btnAvr.TabIndex = 1;
             this.btnAvr.Text = "Averge";
             this.btnAvr.UseVisualStyleBackColor = false;
@@ -59,9 +63,10 @@
             // 
             this.lblAvr.AutoSize = true;
             this.lblAvr.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblAvr.Location = new System.Drawing.Point(70, 185);
+            this.lblAvr.Location = new System.Drawing.Point(52, 150);
+            this.lblAvr.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAvr.Name = "lblAvr";
-            this.lblAvr.Size = new System.Drawing.Size(0, 39);
+            this.lblAvr.Size = new System.Drawing.Size(0, 31);
             this.lblAvr.TabIndex = 2;
             // 
             // label1
@@ -69,23 +74,48 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(11, 32);
+            this.label1.Location = new System.Drawing.Point(8, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(264, 47);
+            this.label1.Size = new System.Drawing.Size(211, 38);
             this.label1.TabIndex = 3;
             this.label1.Text = "Enter numbers:";
             // 
+            // btnMed
+            // 
+            this.btnMed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnMed.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMed.Location = new System.Drawing.Point(58, 215);
+            this.btnMed.Name = "btnMed";
+            this.btnMed.Size = new System.Drawing.Size(88, 45);
+            this.btnMed.TabIndex = 4;
+            this.btnMed.Text = "Median";
+            this.btnMed.UseVisualStyleBackColor = false;
+            this.btnMed.Click += new System.EventHandler(this.btnMed_Click);
+            // 
+            // lblMed
+            // 
+            this.lblMed.AutoSize = true;
+            this.lblMed.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblMed.Location = new System.Drawing.Point(60, 272);
+            this.lblMed.Name = "lblMed";
+            this.lblMed.Size = new System.Drawing.Size(0, 38);
+            this.lblMed.TabIndex = 5;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(694, 366);
+            this.Controls.Add(this.lblMed);
+            this.Controls.Add(this.btnMed);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblAvr);
             this.Controls.Add(this.btnAvr);
             this.Controls.Add(this.txtNum);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -100,6 +130,8 @@
         private System.Windows.Forms.Button btnAvr;
         private System.Windows.Forms.Label lblAvr;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMed;
+        private System.Windows.Forms.Label lblMed;
     }
 }
 
--- Statistics10Edi/Form1.cs
+++ Statistics10Edi/Form1.cs
@@ -29,5 +29,31 @@ namespace Statistics10Edi
 
             lblAvr.Text = ("Avarage: " + avarage);
         }
+
+        private void btnMed_Click(object sender, EventArgs e)
+        {
+            string[] numStrings = txtNum.Text.Split(' ');
+
+            int[] numbers = numStrings.Where(str => ! string.IsNullOrEmpty(str)).Select(str => Convert.ToInt32(str)).ToArray();
+
+            if(numbers.Length > 0)
+            {
+                Array.Sort(numbers);
+
+                double median;
+                if(numbers.Length % 2 == 0)
+                {
+                    int middle1 = numbers.Length / 2 - 1;
+                    int middle2 = numbers.Length / 2;
+                    median = (numbers[middle1] + numbers[middle2]);
+                }
+                else
+                {
+                    int middle = numbers.Length / 2;
+                    median = numbers[middle];
+                }
+                lblMed.Text = ("Median: " + median.ToString());
+            }
+        }
     }
 }
